1a1f94b85a53a1567861ff64b5a7628d9e69d9f1
Thara <91600181+ktea12@users.noreply.github.com>
2021-12-11 22:20:58 +0700

    update

--- Practice1_BloodTypes.js
+++ Practice1_BloodTypes.js
@@ -1,33 +1,3 @@
-const blood = (donor, receiver) => {
-
-    if (donor.toUpperCase() === "A-" && receiver.toUpperCase() === "A+" || "A-" || "AB+" || "AB-") {
-      console.log("blood type is compatible");
-    } else if (donor.toUpperCase() === "A+" && receiver.toUpperCase() === "A+" || "AB+") {
-      console.log("blood type is compatible");
-  
-    } else if (donor.toUpperCase() === "B-" && receiver.toUpperCase() === "B+" || "B-" || "AB+" || "AB-") {
-      console.log("blood type is compatible");
-    } else if (donor.toUpperCase() === "B+" && receiver.toUpperCase() === "B+" || "AB+") {
-      console.log("blood type is compatible");
-    
-    } else if (donor.toUpperCase() === "AB-" && receiver.toUpperCase() === "AB+" || "AB-") {
-        console.log("blood type is compatible");
-    } else if (donor.toUpperCase() === "AB+" && receiver.toUpperCase() === "AB+") {
-        console.log("blood type is compatible");
-    
-    } else if (donor.toUpperCase() === "O-" && receiver.toUpperCase() === "A+" || "A-" || "B+" || "B-" || "AB+" || "AB-" || "O+" || "O-") {
-      console.log("blood type is compatible");
-    } else if (donor.toUpperCase() === "O+" && receiver.toUpperCase() === "A+" || "B+" || "AB+" || "O+") {
-      console.log("blood type is compatible");
-      
-    } else {
-      console.log("blood type is incompatible");
-    }
-  };
-  
-  blood("O+","A+");
-  blood("A-","B-");
-  blood("A-","AB+");
 
 //Creating function blood and assigning 2 parameters: donor and receiver
 const blood = (donor, receiver) => {
