37982f9c3416ec3408ecc17c7a558803da2b3974
Thara <91600181+ktea12@users.noreply.github.com>
2021-12-14 00:44:18 +0700

    Update Practice2_DigitalDecipher.js

--- Practice2_DigitalDecipher.js
+++ Practice2_DigitalDecipher.js
@@ -1,1 +1,16 @@
+const alphabet = ["Error","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+class digitalDecipher {
+    constructor(eMessage, key) {
+        var eMessage = eMessage;
+        
+        var key = key;
+        var keyArr = String(num).split("").map((num) => {
+            return Number(num)}
+        
+        var x = eMessage.map(function(item, index) {
+            return item - keyArr[index];
+            });
+        
+        
+    }};
